a3c46570b247a2dc669b1c88b927e3e526b9d01c
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-05 23:07:54 -0700

    first commit

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -2,7 +2,26 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+    int myValue = 10000;
+
+
+    int myMinIntValue = Integer.MIN_VALUE;
+    int myMaxIntValue = Integer.MAX_VALUE;
+    System.out.println("Integer Min = " + myMinIntValue);
+    System.out.println("Integer Max = " + myMaxIntValue);
+    System.out.println("Busted Max value = " + (myMaxIntValue + 1));
+
+
+    int Max = 2_147_483_647;
+
+    System.out.println(Byte.MAX_VALUE);
+
+    byte myMinByte = Byte.MIN_VALUE;
+    byte myMaxByte = Byte.MAX_VALUE;
+
+    short myMaxShort = Short.MAX_VALUE;
+    short myMinShort = Short.MIN_VALUE;
+  }
 }
