58df3d37db87dc55b0bca6c1916ed6af7ef5162b
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-09 21:34:21 -0700

    first commit

---
+++ Strings/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    //byte
+    //short
+    //int
+    //long
+    //float
+    //double
+    //char
+    //boolean
+
+    String myString = "This is a string";
+
+    System.out.println(myString);
+  }
+}
