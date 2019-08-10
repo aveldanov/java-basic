dbe91b23e088b61898515beffb41259dd7364595
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-09 22:59:21 -0700

    commit

--- charAndBoolean/src/com/company/Main.java
+++ charAndBoolean/src/com/company/Main.java
@@ -5,7 +5,11 @@ public class Main {
   public static void main(String[] args) {
 
     char myChar = 'D';
+    char myUnicode = '\u00A9';
+    System.out.println(myChar);
+    System.out.println(myUnicode);
 
+    boolean myTrueBooleanValue = true;
 
   }
 }
---
+++ keywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        double kilometers=(100*1.609344);
+
+    }
+}
