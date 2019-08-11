2655931e94308680148cfb2fd35e3a8717397155
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-10 20:54:58 -0700

    Java

--- TeenNumberChecker/src/Main.java
+++ TeenNumberChecker/src/Main.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Main {
+  public static void main(String[] args) {
+TeenNumberChecker.isTeen(9,99,19);
+  }
 }
--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class TeenNumberChecker {
+
+  public static boolean isTeen(int num1,int num2, int num3){
+    return true;
+  }
+
+
 }
