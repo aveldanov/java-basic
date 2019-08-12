bb8afc4f550899307dae2c5eb3174f6eb9ead0bc
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-11 21:57:37 -0700

    Java

---
+++ NumberOfDaysInMonth/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+
+
+    System.out.println(NumberOfDaysInMonth.isLeapYear(1601));
+
+    System.out.println(NumberOfDaysInMonth.getDaysInMonth(12,10000));
+  }
+}
