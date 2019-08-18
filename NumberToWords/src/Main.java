ae54d10e2a47310a64dbea264581f00100abe75c
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-17 18:58:49 -0700

    Java

---
+++ NumberToWords/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+
+
+//    NumberToWords.numberToWords(100);
+
+
+//    System.out.println( NumberToWords.reverse(678));
+
+    System.out.println(NumberToWords.getDigitCount(0));
+
+  }
+}
