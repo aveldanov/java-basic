6fab54abae1acade198452a198221e6a83500199
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-17 17:30:59 -0700

    Java

---
+++ LastDigitChecker/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+boolean result=LastDigitChecker.isValid(9);
+
+    System.out.println(result);
+
+   boolean result2= LastDigitChecker.hasSameLastDigit(9,22,71);
+    System.out.println(result2);
+  }
+}
