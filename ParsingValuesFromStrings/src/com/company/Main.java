df7a357c503b75ee68ddc50339f231d6ab8a15b0
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-19 21:22:35 -0700

    Java

---
+++ ParsingValuesFromStrings/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String numberAsString  ="2018";
+
+        System.out.println("numberAsString = "+numberAsString);
+
+
+    }
+}
