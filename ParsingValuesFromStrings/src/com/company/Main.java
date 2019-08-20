324ac2868972439df1ce1f6c38fbc2aa30fc8558
Anton Veldanov <anton.veldanov@gmail.com>
2019-08-19 21:31:11 -0700

    Java

--- ParsingValuesFromStrings/src/com/company/Main.java
+++ ParsingValuesFromStrings/src/com/company/Main.java
@@ -2,12 +2,19 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        String numberAsString  ="2018";
+  public static void main(String[] args) {
+    // write your code here
+    String numberAsString = "2018.125";
 
-        System.out.println("numberAsString = "+numberAsString);
+    System.out.println("numberAsString = " + numberAsString);
+
+    double number = Double.parseDouble(numberAsString);
+    System.out.println("Number = "+number);
+    numberAsString+=1;
+    number+=1;
+    System.out.println("numberAsString = " + numberAsString);
+    System.out.println("Number = "+number);
 
 
-    }
+  }
 }
