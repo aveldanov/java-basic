27da9649b01f348deadf4b22d74d36da144be26e
Anton Veldanov <anton.veldanov@gmail.com>
2019-09-02 15:28:34 -0700

    Java

--- Inheritance/src/com/company/Animal.java
+++ Inheritance/src/com/company/Animal.java
@@ -17,6 +17,14 @@ public class Animal {
     this.weight = weight;
   }
 
+  public void eat(){
+    System.out.println("Animal.eat() was called");
+  }
+
+  public void move(int speed){
+    System.out.println("Animal.move() called. Animal is moving at "+speed);
+  }
+
   public String getName() {
     return name;
   }
